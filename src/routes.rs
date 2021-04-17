8a4e55ca540a11f06ef6601ce3f20bb537d31a17
Hagen Schink <hagen.schink@akelius.de>
2021-04-17 22:07:09 +0200

    Test root

--- src/routes.rs
+++ src/routes.rs
@@ -14,3 +14,23 @@ pub fn index() -> JsonValue {
         "version": env!("CARGO_PKG_VERSION")
     })
 }
+
+#[cfg(test)]
+mod test {
+    use rocket;
+    use rocket::routes;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_world() {
+        let rocket = rocket::ignite().mount("/", routes![super::index]);
+        let client = Client::new(rocket).expect("valid rocket instance");
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some(format!("{{\"name\":\"{}\",\"version\":\"{}\"}}"
+            , env!("CARGO_PKG_NAME")
+            , env!("CARGO_PKG_VERSION")
+        )));
+    }
+}
