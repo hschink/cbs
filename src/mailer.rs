6a735563005e1655fb209b45572a6c94eb17a064
Hagen Schink <hagen.schink@akelius.de>
2021-05-02 21:06:17 +0200

    Send emails via BCC and support more receivers

--- src/mailer.rs
+++ src/mailer.rs
@@ -13,14 +13,15 @@ use lettre::{Message, SmtpTransport, Transport};
 use crate::database::models::Booking;
 use crate::mailer::errors::MailerError;
 
-fn send_mail_from_backend(to: Vec<&String>, subject: String, body: String) -> Result<Response, Error> {
+fn send_mail_from_backend(to: Vec<&str>, subject: String, body: String) -> Result<Response, Error> {
     let mut message_builder = Message::builder();
 
     message_builder = message_builder.from(env::var("EMAIL_FROM").unwrap().parse().unwrap())
+        .to(to[0].parse().unwrap())
         .subject(format!("[{}] {}", env::var("EMAIL_SUBJECT_PREFIX").unwrap(), subject));
 
-    for x in to {
-        message_builder = message_builder.to(x.parse().unwrap());
+    for x in &to[1..] {
+        message_builder = message_builder.bcc(x.parse().unwrap());
     }
 
     let email = message_builder.body(body).unwrap();
@@ -44,7 +45,10 @@ pub fn is_mail_config_available() -> bool {
 }
 
 pub fn send_startup_mail() -> Result<Response, MailerError> {
-    let result = send_mail_from_backend(vec![&env::var("EMAIL_TO").unwrap()],
+    let email_to = env::var("EMAIL_TO").unwrap();
+    let to = email_to.split(",").collect::<Vec<_>>();
+
+    let result = send_mail_from_backend(to,
         "Launch".to_string(),
         "Cargobike share backend is about to launch!".to_string()
     );
@@ -60,8 +64,8 @@ pub fn send_rent_mail(booking: &Booking) -> Result<Response, MailerError> {
     let start_date = booking.start_timestamp.format("%Y-%m-%d");
     let end_date = booking.end_timestamp.format("%Y-%m-%d");
     let subject = format!("New rent from {} to {}", start_date, end_date);
-    let initial_to = env::var("EMAIL_TO").unwrap();
-    let mut to = vec![&initial_to];
+    let email_to = env::var("EMAIL_TO").unwrap();
+    let mut to = email_to.split(",").collect::<Vec<_>>();
 
     if booking.email.is_some() {
         to.push(booking.email.as_ref().unwrap());
