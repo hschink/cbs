5ffdc6047d6697c440a6c755900058efd1429dda
Hagen Schink <hagen.schink@akelius.de>
2021-06-25 22:34:40 +0200

    Extend database schema

---
+++ migrations/2021-06-25-193640_supporters/down.sql
@@ -0,0 +1,3 @@
+DROP    TABLE   IF  EXISTS supporter_translatables          CASCADE;
+DROP    TABLE   IF  EXISTS supporters                       CASCADE;
+DROP    TABLE   IF  EXISTS supporter_types                  CASCADE;
--- src/schema.rs
+++ src/schema.rs
@@ -36,6 +36,34 @@ table! {
 }
 
 table! {
+    supporter_translatables (id) {
+        id -> Int4,
+        supporter_id -> Int4,
+        locale -> Varchar,
+        title -> Varchar,
+        description -> Nullable<Text>,
+        url -> Nullable<Varchar>,
+        logo_url -> Nullable<Varchar>,
+        logo_width -> Nullable<Int2>,
+        logo_height -> Nullable<Int2>,
+    }
+}
+
+table! {
+    supporter_types (id) {
+        id -> Int4,
+        title -> Varchar,
+    }
+}
+
+table! {
+    supporters (id) {
+        id -> Int4,
+        supporter_type_id -> Nullable<Int4>,
+    }
+}
+
+table! {
     token_challenge_translatables (id) {
         id -> Int4,
         token_challenge_id -> Int4,
@@ -64,6 +92,8 @@ joinable!(bike_translatables -> bikes (bike_id));
 joinable!(rent_details -> rents (rent_id));
 joinable!(rents -> bikes (bike_id));
 joinable!(rents -> tokens (token_id));
+joinable!(supporter_translatables -> supporters (supporter_id));
+joinable!(supporters -> supporter_types (supporter_type_id));
 joinable!(token_challenge_translatables -> token_challenges (token_challenge_id));
 
 allow_tables_to_appear_in_same_query!(
@@ -71,6 +101,9 @@ allow_tables_to_appear_in_same_query!(
     bikes,
     rent_details,
     rents,
+    supporter_translatables,
+    supporter_types,
+    supporters,
     token_challenge_translatables,
     token_challenges,
     tokens,
